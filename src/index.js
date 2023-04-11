830307a7f1492bdc688cb0649814bf506486f0d6
apav2086 <apav2086@gmail.com>
2023-04-10 20:07:10 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+/*fetch('https://restcountries.com/v3.1/name/{name}')
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    });*/
